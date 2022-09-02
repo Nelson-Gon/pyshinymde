9d7a632bda9d5063f5be413b537cebd93814ed19
NelsonGon <gonzabato@hotmail.com>
2022-09-02 21:31:25 +0200

    Fix reqiuirements file

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
+from pyshinymde.version import __version__
 from setuptools import setup, find_packages
-import os 
-# Change project to project name here 
-# Choose relevant license as required, add necessary install_requires 
-# Automate the process of reading from requirements.txt 
-install_requires_list =  []
-reqs_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt")
+import os
+# Change project to project name here
+# Choose relevant license as required, add necessary install_requires
+# Automate the process of reading from requirements.txt
+install_requires_list = []
+reqs_file = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "requirements.txt")
 with open(reqs_file, "r") as input_file:
     for line in input_file:
-          # rstrip to remove the newline character after appending to an empty list. 
-          install_requires_list.append(line.rstrip())
+        # rstrip to remove the newline character after appending to an empty list.
+        install_requires_list.append(line.rstrip())
+install_requires_list.append(
+    "shiny @ git+https://github.com/rstudio/py-shiny.git@816846d377c2e4973c8b4fde81f88ad3600a2d80#egg=shiny")
 
-
-from pyshinymde.version import __version__
-
-link_to_use = "https://github.com/Nelson-Gon/pyshinymde/archive/refs/tags/v"+__version__+".zip"
+print(install_requires_list)
+link_to_use = "https://github.com/Nelson-Gon/pyshinymde/archive/refs/tags/v" + \
+    __version__+".zip"
 setup(name='project',
       version=__version__,
       description="Python Citations Generator",
@@ -29,4 +32,3 @@ setup(name='project',
       install_requires=install_requires_list,
       python_requires='>=3.6',
       zip_safe=False)
-
